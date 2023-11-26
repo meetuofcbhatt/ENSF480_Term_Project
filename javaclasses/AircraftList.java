1e22e0a927ae02550858b40ddebaec456acfd35b
vsokolosky <113938464+vsokolosky@users.noreply.github.com>
2023-11-25 20:30:19 -0700

    Update AircraftList.java

--- javaclasses/AircraftList.java
+++ javaclasses/AircraftList.java
@@ -1,19 +1,23 @@
-package ensf480final;
+package javaclasses;
 
 import java.util.ArrayList;
 
 public class AircraftList {
-	private static DatabaseSingleton onlyInstance;
+	private static AircraftList onlyInstance;
     private ArrayList<Aircraft> aircrafts; 
     
     private AircraftList() {
     	
     }
 
-    public static DatabaseSingleton getOnlyInstance(){
-        if(onlyInstance ==null){
-            onlyInstance = new DatabaseSingleton();
+    public static AircraftList getOnlyInstance(){
+        if (onlyInstance == null){
+            onlyInstance = new AircraftList();
         }
         return onlyInstance;
+    }
+    
+    public void add(Aircraft a) {
+    	aircrafts.add(a);
     }
 }
