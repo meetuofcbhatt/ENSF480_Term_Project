f38848b952e0ad2035ff714d589974a0c937d12e
vsokolosky <113938464+vsokolosky@users.noreply.github.com>
2023-11-26 17:15:39 -0700

    Update Flights.java, time/date/length may need to be changed

--- javaclasses/Flights.java
+++ javaclasses/Flights.java
@@ -1,116 +1,112 @@
 package javaclasses;
 
-public class Flights {
+public class Flight {
 
-    private int flightID;
-    public String  destination;
-    public String  origin;
-    public String  flightName;
-    public int date;
+    private String flightName;
+    private String destination;
+    private String origin;
+    private int flightID;	// should be unique
+    public int date;	// departure date
+    public int time;	// departure time
+    public int length;	// length of the flight
     public int crewID;
     public int aircraftID;
-    public String aircraft;
-
-
-       public Flights(int flightID, String  destination, String  origin, String  flightName, int date, int crewID, int aircraftID, String aircraft) {
-
-        /**
-         * This is the Task constructor that creates a task object based on the parameters passed into the constructor
-
-         *
-         * @param ID id of the task, type is integer
-         * @param description what the task is, type is string
-         * @param duration time taken for the task, type is int
-         * @param maxWin the maximum hourly window the task needs to be scheduled in, type is int
-         *
-         */
+    
+    /**
+     * This is the Flight constructor that creates a Flight object based on the parameters passed into the constructor
+     *
+     * @param flightName name of the flight, type is String
+     * @param destination where the flight is going, type is String
+     * @param origin where the flight takes off from, type is String
+     * @param date day when the flight departs, type is int
+     * @param time time of departure, type is int
+     * @param flightID unique ID to identify flights, type is int
+     * @param crewID identifies the crew working on that flight, type is int
+     * @param aircraftID identifies the aircraft used for that flight, type is int
+     */
+    public Flight(String flightName, String  destination, String  origin, int date, int time, int flightID, int crewID, int aircraftID) {
+        
 
         this.flightID = flightID;
         this.destination = destination;
         this.origin = origin;
         this.flightName =flightName;
         this.date = date;
+        this.time = time;
         this.crewID= crewID;
         this.aircraftID = aircraftID;
-        this.aircraft =aircraft;
 
     }
 
-    public int getFlightID(){
-
-        /**
-         * Gets the TaskID that is associated with the task it was called from
-         * @return taskID, type is int
-         */
-        return flightID;
-    }
-    public String getDestination(){
-        /**
-         * Gets the duration that is associated with the task it was called from
-         * @return duration, type is int
-         */
-        return destination;
-    }
-
-    public String getOrigin(){
-
-        /**
-         * Gets the Max windoe that is associated with the task it was called from
-         * @return maxWin, type is int
-         */
-        return origin;
-    }
-    public String getFlightName(){
-
-
-        /**
-         * Gets the description that is associated with the task it was called from
-         * @return description, type is String
-         */
-        return flightName;
-    }
-
-    public int getDate(){
-
-
-        /**
-         * Gets the description that is associated with the task it was called from
-         * @return description, type is String
-         */
-        return date;
-    }
-
-
-    public int getCrewID(){
-
-
-        /**
-         * Gets the description that is associated with the task it was called from
-         * @return description, type is String
-         */
-        return crewID;
+    /**
+     * Gets the flightName variable of a Flight object
+     * @return flightName, type is String
+     */
+    public String getFlightName() {
+    	return flightName;
     }
     
-
-
-    public int getAircraftID(){
-
-
-        /**
-         * Gets the description that is associated with the task it was called from
-         * @return description, type is String
-         */
-        return aircraftID;
+    /**
+     * Gets the destination variable of a Flight object
+     * @return destination, type is String
+     */
+    public String getDestination() {
+    	return destination;
     }
-
-
-    public String getAircraft(){
-
-
-        /**
-         * Gets the description that is associated with the task it was called from
-         * @return description, type is String
-         */
-        return aircraft;
+    
+    /**
+     * Gets the origin variable of a Flight object
+     * @return origin, type is String
+     */
+    public String getOrigin() {
+    	return origin;
+    }
+    
+    /**
+     * Gets the flightID variable of a Flight object
+     * @return flightID, type is int
+     */
+    public int getFlightID() {
+    	return flightID;
+    }
+    
+    /**
+     * Gets the date variable of a Flight object
+     * @return date, type is int
+     */
+    public int getDate() {
+    	return date;
+    }
+    
+    /**
+     * Gets the time variable of a Flight object
+     * @return time, type is int
+     */
+    public int getTime() {
+    	return time;
+    }
+    
+    /**
+     * Gets the length variable of a Flight object
+     * @return length, type is int
+     */
+    public int length() {
+    	return length;
+    }
+    
+    /**
+     * Gets the ID of the crew associated with a Flight object
+     * @return crewID, type is int
+     */
+    public int crewID() {
+    	return crewID;
+    }
+    
+    /**
+     * Gets the ID of the aircraft associated with a Flight object
+     * @return aircraftID, type is int
+     */
+    public int aircraftID() {
+    	return aircraftID;
     }
 }
